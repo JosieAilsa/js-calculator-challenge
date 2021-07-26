de4d67e2791df73b0c673a70a3d1da07b3b4d7fc
Your Name <you@example.com>
2021-07-26 15:27:14 +0100

    styling calc

---
+++ script.js
@@ -0,0 +1,20 @@
+// Reference all the possible buttons from the DOM into global scope 
+// Numbers 
+let one = document.querySelector("#1");
+let two = document.querySelector("#2");
+let three = document.querySelector("#3");
+let four = document.querySelector("#4");
+let five = document.querySelector("#5");
+let six = document.querySelector("#6");
+let seven = document.querySelector("#7");
+let eight = document.querySelector("#8");
+let nine = document.querySelector("#9");
+//Operators 
+let answer = document.querySelector("#answer");
+let plus = document.querySelector("#plus");
+let divide = document.querySelector("#divide");
+let clear = document.querySelector("#clear");
+let minus = document.querySelector("#minus");
+let multiply = document.querySelector("#*");
+let decimal = document.querySelector("#.");
+
